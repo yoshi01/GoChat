57dd2052ef8276b962a46f869b26a0ed13eaadcc
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2020-04-18 15:08:58 +0900

    feat: chart app

--- app/main.go
+++ app/main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
-	"log"
-	"net/http"
+  "log"
+  "net/http"
+  "sync"
+  "flag"
+  "text/template"
+  "path/filepath"
+  "app/trace"
+  "os"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう!
-				</body>
-			</html>
-		`))
-	})
+type templateHandler struct {
+  once sync.Once
+  filename string
+  templ *template.Template
+}
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  t.once.Do(func() {
+    t.templ =
+      template.Must(template.ParseFiles(filepath.Join("templates",
+        t.filename)))
+  })
+  t.templ.Execute(w, r)
+}
+
+func main() {
+  var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+  flag.Parse()
+  r := newRoom()
+  r.tracer = trace.New(os.Stdout)
+  http.Handle("/", &templateHandler{filename: "chat.html"})
+  http.Handle("/room", r)
+
+  go r.run()
+  log.Println("Webサーバーを開始します。ポート: ", *addr)
+
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
---
+++ app/trace/tracer_test.go
@@ -0,0 +1,23 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("こんにちは、trace")
+		if buf.String() != "こんにちは、trace\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
+
+func TestOff(t *testing.T) {
+	var silentTracer Tracer = Off()
+	silentTracer.Trace("データ")
+}

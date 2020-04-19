c2c459c4135eef1df3757080fbae816247d6697d
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2020-04-19 16:23:27 +0900

    feat: auth

---
+++ app/auth.go
@@ -0,0 +1,37 @@
+package main
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"log"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		h.next.ServeHTTP(w, r)
+	}
+}
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+}
--- app/main.go
+++ app/main.go
@@ -31,7 +31,9 @@ func main() {
   flag.Parse()
   r := newRoom()
   r.tracer = trace.New(os.Stdout)
-  http.Handle("/", &templateHandler{filename: "chat.html"})
+  http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+  http.Handle("/login", &templateHandler{filename: "login.html"})
+  http.HandleFunc("/auth/", loginHandler)
   http.Handle("/room", r)
 
   go r.run()
